2d9100ee984d460cc6a2a1fabddcb6d156b9e16c
Nelson Wing <dwing998@mtroyal.ca>
2025-10-04 14:52:27 -0600

    rigidBodyUpdate

--- P1_Character.gd
+++ P1_Character.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 func get_input():
 	var input_direction = Input.get_vector("P1 Left", "P1 Right", "P1 Up", "P1 Down")
 	velocity = input_direction * speed
+	
 
 func _physics_process(delta):
 	get_input()
