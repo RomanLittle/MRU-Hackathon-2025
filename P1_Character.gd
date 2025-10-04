896203aa4b51fd11fbb00ac58b28a848fff48443
Nelson Wing <dwing998@mtroyal.ca>
2025-10-04 11:45:00 -0600

    hi

---
+++ P1_Character.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+@export var speed = 400
+
+func get_input():
+	var input_direction = Input.get_vector("P1 Left", "P1 Right", "P1 Up", "P1 Down")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
