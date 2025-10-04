c7f0b41f9bd24fc1f60e732362829be0949c9b75
Nelson Wing <dwing998@mtroyal.ca>
2025-10-04 15:40:12 -0600

    chainPhysics

--- rigid_body_2d.gd
+++ rigid_body_2d.gd
@@ -7,13 +7,13 @@ func _physics_process(delta):
 	# Get input and update move_direction
 	move_direction = Vector2.ZERO
 	if Input.is_action_pressed("P1 Right"):
-		move_direction.x += 50
+		move_direction.x += 1
 	if Input.is_action_pressed("P1 Left"):
-		move_direction.x -= 50
+		move_direction.x -= 1
 	if Input.is_action_pressed("P1 Up"):
-		move_direction.y -= 50
+		move_direction.y -= 1
 	if Input.is_action_pressed("P1 Down"):
-		move_direction.y += 50
+		move_direction.y += 1
 	
 	# Normalize the direction to prevent faster diagonal movement
 	move_direction = move_direction.normalized()
